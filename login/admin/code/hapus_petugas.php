e4619b15a2797bbac16abdfed3157acf30c16321
ramanovaris <ramaanovariss@gmail.com>
2020-02-13 21:10:07 +0800

    Fix Hapus petugas

--- login/admin/code/hapus_petugas.php
+++ login/admin/code/hapus_petugas.php
@@ -3,7 +3,12 @@ error_reporting(0);
 session_start();
 include "../config/koneksi.php";
 
+$query_mysql = mysql_query("SELECT * FROM petugas WHERE id_petugas='$_GET[id]'")or die(mysql_error());
+$data = mysql_fetch_array($query_mysql);
+// var_dump($data['id_akun']);
+// die();
 $hapus = mysql_query("DELETE FROM petugas WHERE id_petugas='$_GET[id]'")or die(mysql_error());
+$hapus = mysql_query("DELETE FROM akun WHERE kode_akun='$data[id_akun]'")or die(mysql_error());
 
 if($hapus){
     echo "<script>
